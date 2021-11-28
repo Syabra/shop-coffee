48a17eab3784922928277835d2669558d8792a41
Syabra <Syabra.bel@gmail.com>
2021-11-28 15:52:07 +0300

    A controllers -home -Coffee

---
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -0,0 +1,28 @@
+﻿using CoffeeShop.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class CoffeeController : Controller
+    {
+        private readonly IAllCoffee _allCoffee;
+        private readonly ICoffeeCategory _allCategories;
+
+        public CoffeeController(IAllCoffee iAllCoffee, ICoffeeCategory iCoffeeCategory)
+        {
+            _allCoffee = iAllCoffee;
+            _allCategories = iCoffeeCategory;
+        }
+        
+        public ViewResult List()
+        {
+            var coffee = _allCoffee;
+            return View(coffee);
+        }
+    }
+}
+ 
--- CoffeeShop/Controllers/HomeController.cs
+++ CoffeeShop/Controllers/HomeController.cs
@@ -1,37 +1,28 @@
-﻿using CoffeeShop.Models;
+﻿using CoffeeShop.Data.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using CoffeeShop.ViewModels;
 
 namespace CoffeeShop.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
+        private IAllCoffee _coffeeRep;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IAllCoffee coffeeRep)
         {
-            _logger = logger;
+            _coffeeRep = coffeeRep;
         }
 
-        public IActionResult Index()
+        public ViewResult Index()
         {
-            return View();
-        }
+            var homeCoffee = new HomeViewModel
+            {
+                FavCoffee = _coffeeRep.getCoffee
+            };
 
-        public IActionResult Privacy()
-        {
-            return View();
+            return View(homeCoffee);
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
